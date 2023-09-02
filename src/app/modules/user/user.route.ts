d69a0cdea75c5653b9ef89f10286940dc7a898fb
Fahad <205fahadmohammad@gmail.com>
2023-09-02 17:02:45 +0600

    role guard added to all user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.get('/:id', UserController.getSingleUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
-router.patch('/:id', UserController.updateUser);
-router.delete('/:id', UserController.deleteUser);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(UserValidation.update),
+  UserController.updateUser
+);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
 export const UserRoutes = router;
